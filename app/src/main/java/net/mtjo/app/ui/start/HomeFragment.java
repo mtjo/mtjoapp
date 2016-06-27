188bca2cce4bcfe26f94e5cf7935e224a510b5f6
weihongjiang <weihongjiang@wbiao.cn>
2016-06-27 18:14:04 +0800

    add gridview

--- app/src/main/java/net/mtjo/app/ui/start/HomeFragment.java
+++ app/src/main/java/net/mtjo/app/ui/start/HomeFragment.java
@@ -8,7 +8,6 @@ import net.mtjo.app.api.HttpPostManager;
 import net.mtjo.app.ui.base.WebViewActivity;
 
 import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -16,12 +15,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.TextView;
+import android.widget.GridView;
+import android.widget.ListAdapter;
+import android.widget.ScrollView;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
 import com.daimajia.slider.library.*;
-import com.daimajia.slider.library.Animations.DescriptionAnimation;
 import com.daimajia.slider.library.SliderTypes.BaseSliderView;
 import com.daimajia.slider.library.SliderTypes.TextSliderView;
 import com.daimajia.slider.library.Transformers.AccordionTransformer;
@@ -46,11 +46,29 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 public class HomeFragment extends Fragment implements OnClickListener, BaseSliderView.OnSliderClickListener {
     private Activity aty;
     SliderLayout sliderShow;
+
+    private  ArrayList<Map<String, Object>> data_list;
+
+    private GridView gview;
+
+    private int[] icon = { R.drawable.sns_icon_1, R.drawable.sns_icon_2,
+            R.drawable.sns_icon_6, R.drawable.sns_icon_19, R.drawable.sns_icon_22,
+            R.drawable.sns_icon_23, R.drawable.sns_icon_24, R.drawable.ic_launcher,
+            R.drawable.ic_launcher, R.drawable.ic_launcher, R.drawable.ic_launcher,
+            R.drawable.ic_launcher };
+    private String[] iconName = { "通讯录", "日历", "照相机", "时钟", "游戏", "短信", "铃声",
+            "设置", "语音", "天气", "浏览器", "视频" };
+
+
 
     private JSONArray homeSlides = null;
 
@@ -62,7 +80,10 @@ public class HomeFragment extends Fragment implements OnClickListener, BaseSlide
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.home_fragment, container, false);
+
     }
+
+
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
@@ -71,6 +92,13 @@ public class HomeFragment extends Fragment implements OnClickListener, BaseSlide
         aty = getActivity();
         initView();
         //showTip();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        ScrollView scrollview = ((ScrollView)aty.findViewById(R.id.scrollView));
+        scrollview.scrollTo(0,0);
     }
 
     private void initView() {
@@ -84,69 +112,10 @@ public class HomeFragment extends Fragment implements OnClickListener, BaseSlide
                     public void onSuccess(Object t) {
 
                         homeSlides = this.getJsonArray();
+                        BaseTransformer transformer = transformer() ;
                         for (int i = 0; i < homeSlides.length(); i++) {
                             try {
                                 JSONObject oj = homeSlides.getJSONObject(i);
-                                BaseTransformer transformer ;
-                                Random rand = new Random();
-
-                                switch (rand.nextInt(17)){
-                                    case 0:
-                                        transformer= new AccordionTransformer();
-                                        break;
-                                    case 1:
-                                        transformer= new BackgroundToForegroundTransformer();
-                                        break;
-                                    /*case 2:
-                                        transformer= new BaseTransformer();
-                                        break;*/
-                                    case 3:
-                                        transformer= new CubeInTransformer();
-                                        break;
-                                    case 4:
-                                        transformer= new DefaultTransformer();
-                                        break;
-                                    case 5:
-                                        transformer= new DepthPageTransformer();
-                                        break;
-                                    case 6:
-                                        transformer= new FadeTransformer();
-                                        break;
-                                    case 7:
-                                        transformer= new FlipHorizontalTransformer();
-                                        break;
-                                    case 8:
-                                        transformer= new FlipPageViewTransformer();
-                                        break;
-                                    case 9:
-                                        transformer= new ForegroundToBackgroundTransformer();
-                                        break;
-                                    case 10:
-                                        transformer= new RotateDownTransformer();
-                                        break;
-                                    case 11:
-                                        transformer= new RotateUpTransformer();
-                                        break;
-                                    case 12:
-                                        transformer= new StackTransformer();
-                                        break;
-                                    case 13:
-                                        transformer= new TabletTransformer();
-                                        break;
-                                    case 14:
-                                        transformer= new ZoomInTransformer();
-                                        break;
-                                    case 15:
-                                        transformer= new ZoomOutSlideTransformer();
-                                        break;
-                                    case 16:
-                                        transformer= new ZoomOutTransformer();
-                                        break;
-                                    default:
-                                        transformer= new AccordionTransformer();
-                                        break;
-
-                                }
 
 
                                 TextSliderView textSliderView = new TextSliderView(aty);
@@ -180,6 +149,18 @@ public class HomeFragment extends Fragment implements OnClickListener, BaseSlide
                 }, null, null);
 
 
+        gview = (GridView) aty.findViewById(R.id.gridView);
+        //新建List
+        data_list = new ArrayList<Map<String, Object>>();
+        //获取数据
+        getData();
+        //新建适配器
+        String [] from ={"image","text"};
+        int [] to = {R.id.image,R.id.text};
+        SimpleAdapter sim_adapter = new SimpleAdapter(aty, data_list, R.layout.gridview_item, from, to);
+        //配置适配器
+        gview.setAdapter(sim_adapter);
+        setListViewHeightBasedOnChildren(gview);
     }
 
     @Override
@@ -215,8 +196,115 @@ public class HomeFragment extends Fragment implements OnClickListener, BaseSlide
 
     @Override
     public void onSliderClick(BaseSliderView slider) {
-        Toast.makeText(aty, slider.getBundle().get("slide_url") + "", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(aty, slider.getBundle().get("slide_url") + "", Toast.LENGTH_SHORT).show();
         WebViewActivity.WebView(aty, slider.getBundle().get("slide_url") + "","文章详情");
+    }
+/**
+ * BaseTransformer transformer
+ * */
+    public BaseTransformer transformer() {
+        Random rand = new Random();
+        BaseTransformer transformer;
+
+        switch (rand.nextInt(17)) {
+            case 0:
+                transformer = new AccordionTransformer();
+                break;
+            case 1:
+                transformer = new BackgroundToForegroundTransformer();
+                break;
+                                    /*case 2:
+                                        transformer= new BaseTransformer();
+                                        break;*/
+            case 3:
+                transformer = new CubeInTransformer();
+                break;
+            case 4:
+                transformer = new DefaultTransformer();
+                break;
+            case 5:
+                transformer = new DepthPageTransformer();
+                break;
+            case 6:
+                transformer = new FadeTransformer();
+                break;
+            case 7:
+                transformer = new FlipHorizontalTransformer();
+                break;
+            case 8:
+                transformer = new FlipPageViewTransformer();
+                break;
+            case 9:
+                transformer = new ForegroundToBackgroundTransformer();
+                break;
+            case 10:
+                transformer = new RotateDownTransformer();
+                break;
+            case 11:
+                transformer = new RotateUpTransformer();
+                break;
+            case 12:
+                transformer = new StackTransformer();
+                break;
+            case 13:
+                transformer = new TabletTransformer();
+                break;
+            case 14:
+                transformer = new ZoomInTransformer();
+                break;
+            case 15:
+                transformer = new ZoomOutSlideTransformer();
+                break;
+            case 16:
+                transformer = new ZoomOutTransformer();
+                break;
+            default:
+                transformer = new AccordionTransformer();
+                break;
+
+        }
+        return transformer;
+    }
+
+    public List<Map<String, Object>> getData(){
+        //cion和iconName的长度是相同的，这里任选其一都可以
+        for(int i=0;i<icon.length;i++){
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("image", icon[i]);
+            map.put("text", iconName[i]);
+            data_list.add(map);
+        }
+
+        return data_list;
+    }
+
+    public static void setListViewHeightBasedOnChildren(GridView listView) {
+        // 获取listview的adapter
+        ListAdapter listAdapter = listView.getAdapter();
+        if (listAdapter == null) {
+            return;
+        }
+        // 固定列宽，有多少列
+        int col = 3;// listView.getNumColumns();
+        int totalHeight = 0;
+        // i每次加4，相当于listAdapter.getCount()小于等于4时 循环一次，计算一次item的高度，
+        // listAdapter.getCount()小于等于8时计算两次高度相加
+        for (int i = 0; i < listAdapter.getCount(); i += col) {
+            // 获取listview的每一个item
+            View listItem = listAdapter.getView(i, null, listView);
+            listItem.measure(0, 0);
+            // 获取item的高度和
+            totalHeight += listItem.getMeasuredHeight();
+        }
+
+        // 获取listview的布局参数
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        // 设置高度
+        params.height = totalHeight;
+        // 设置margin
+        ((ViewGroup.MarginLayoutParams) params).setMargins(10, 10, 10, 10);
+        // 设置参数
+        listView.setLayoutParams(params);
     }
 
 }
