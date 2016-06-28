d36ad75af09a2f835c2c416bbb4597d86a371267
= <weihongjiang@wbiao.cn>
2016-06-28 20:15:28 +0800

    fix git verify code

--- app/src/main/java/net/mtjo/app/ui/my/LoginActivity.java
+++ app/src/main/java/net/mtjo/app/ui/my/LoginActivity.java
@@ -41,74 +41,39 @@ public class LoginActivity extends BaseActivity {
     }
     private void initView(){
         imageView = (ImageView)findViewById(R.id.imgcode);
-        imageView.setImageBitmap(RandomCode.getInstance().createBitmap());
+        get_code();
 
         imageView.setOnClickListener(new View.OnClickListener() {
 
-            public void onClick(View v) {
-
-                HttpPostManager.getVerifyCode(
-                        new StringCallBack() {
-
-                            @Override
-                            public void onSuccess(Object t) {
-
-                               String code = this.getStrContent();
-                                imageView.setImageBitmap(RandomCode.getInstance().createBitmap(code));
-
-                            }
-
-                            @Override
-                            public void onFailure(Throwable t, int errorNo, String strMsg) {
-                                Toast.makeText(LoginActivity.this, strMsg, Toast.LENGTH_SHORT).show();
-                            }
-                        }, null, null);
-
-                System.out.println("验证码："+RandomCode.getInstance().getCode());
+            public void onClick(View v) { get_code();
             }
         });
 
-
-
-
-
-
-
-
     }
 
-    /**
-     * 获取网落图片资源
-     * @param url
-     * @return
-     */
-    public static Bitmap getHttpBitmap(String url){
-        URL myFileURL;
-        Log.i(TAG, url);
-        Bitmap bitmap=null;
-        try{
-            myFileURL = new URL(url);
-            //获得连接
-            HttpURLConnection conn=(HttpURLConnection)myFileURL.openConnection();
-            //设置超时时间为6000毫秒，conn.setConnectionTiem(0);表示没有时间限制
-            conn.setConnectTimeout(6000);
-            //连接设置获得数据流
-            conn.setDoInput(true);
-            //不使用缓存
-            conn.setUseCaches(false);
-            //这句可有可无，没有影响
-            //conn.connect();
-            //得到数据流
-            InputStream is = conn.getInputStream();
-            //解析得到图片
-            bitmap = BitmapFactory.decodeStream(is);
-            //关闭数据流
-            is.close();
-        }catch(Exception e){
-            e.printStackTrace();
-        }
+    public void get_code(){
+        String code;
 
-        return bitmap;
+
+        HttpPostManager.getVerifyCode(
+                new StringCallBack() {
+
+                    @Override
+                    public void onSuccess(Object t) {
+                        String code = this.getStrContent();
+                        imageView.setImageBitmap(RandomCode.getInstance().createBitmap(code));
+
+
+
+                    }
+
+                    @Override
+                    public void onFailure(Throwable t, int errorNo, String strMsg) {
+                        Toast.makeText(LoginActivity.this, strMsg, Toast.LENGTH_SHORT).show();
+                    }
+                }, null, null);
+
+        System.out.println("验证码："+RandomCode.getInstance().getCode());
 
     }
 
